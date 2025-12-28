1b0965dcf11fde0807412737e060b5264bb180e6
David Brown <davidjvbrown.md@gmail.com>
2025-12-28 17:03:35 -0600

    Added creatinine mg/dL to umol/L conversion factor

--- src/constants.rs
+++ src/constants.rs
@@ -7,3 +7,6 @@ pub const GLU_MMOLL_TO_MGDL: f64 = 18.0;
 
 /// Multiply by this factor to convert glucose mg/dL to mmol/L.
 pub const GLU_MGDL_TO_MMOLL: f64 = 1.0 / GLU_MMOLL_TO_MGDL;
+
+/// Multiply by this factor to convert creatinine mg/dL to umol/L
+pub const SCR_MGDL_TO_UMOLL: f64 = 88.4;
